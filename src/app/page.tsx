28a6e3c717822002795fbecf43a8e55a0ba47510
sharyu08 <sharayumate.22@stvincentngp.edu.in>
2025-09-28 10:47:57 +0530

    Add New Arrival page and update Shop Now button to redirect to new arrivals

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default function HomePage() {
                 </span>
               </div>
               <Link
-                href="/shop"
+                href="/new-arrival"
                 className="inline-block text-xl font-bold px-8 py-3 rounded-md shadow transition"
                 style={{
                   backgroundColor: "#A0937D",
